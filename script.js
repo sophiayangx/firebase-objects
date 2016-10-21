3d0b58c340b27fb874bdbbdf5e18b3b3d1d59367
sophiayangx <sopyang@nuevaschool.org>
2016-10-21 08:49:11 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 onNewListItem("product-list", function(product) {
   console.log(product.image)
   var nameHeader = $("<h1 class='product-name'>" + product.name + "</h1>")
-  var image = $("<img class='product-image'>").attr("src", "http://pix.iemoji.com/images/emoji/apple/ios-9/256/thinking-face.png")  
+  var image = $("<img class='product-image'>").attr("src", "http://pix.iemoji.com/images/emoji/apple/ios-9/256/thinking-face.png") 
 
   $(".product-list-container").append(nameHeader, image)
 })
